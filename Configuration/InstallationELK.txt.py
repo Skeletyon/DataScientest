367687e8f6f5fea8f68a62d86f83e630b9c88084
nguyenhn <hnhan.nguyen@gmail.com>
2024-03-30 13:26:00 +0100

    EN mode http

---
+++ Configuration/InstallationELK.txt.py
@@ -0,0 +1,7 @@
+# Installation ELK.
+
+Installation ELK en mode HTTP voir le fichier yml joint
+
+Pour modifier le password, utiliser la commande :
+bin/elasticsearch-setup-passwords auto -u <username>
+
