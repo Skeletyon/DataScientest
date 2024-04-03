6ea2357a81d6b2bce7be437eba71d3dcc05ea2be
nguyenhn <hnhan.nguyen@gmail.com>
2024-04-03 23:05:38 +0200

    Nouvelle version en ajoutant Sentiment, mots positifs, mots negatis, lang

--- elastic/chargement.py
+++ elastic/chargement.py
@@ -1,6 +1,7 @@
 from elasticsearch import Elasticsearch
 import json
 import datetime
+from LibSatisfaction import mots_pos_neg, detect_language
 
 # Définir l'URL de la base Elasticsearch
 url = "http://localhost:9200"
@@ -8,10 +9,27 @@ url = "http://localhost:9200"
 # Définir le nom d'utilisateur et le mot de passe
 username = "elastic"
 password = "changeme"
-index_name = "satisfactionclients_stopword"
+index_name = "satisfactionclients"
 jsonFile = "satisfactionWonderbox.json"
 
 #
+# # Mapping final avec langue, Mots positifs, Mot Négatifs, Sentiments
+#     mapping = {
+#         "mappings": {
+#             "properties": {
+#                 "Personne": {"type": "text"},
+#                 "Commentaire": {"type": "text" , "fielddata": True},
+#                 "Rating" : {"type": "float"},
+#                 "Date": {"type": "date"},
+#                 "Reponse": {"type": "text", "fielddata": True},
+#                 "Langue": {"type": "text", "fielddata": True},
+#                 "MotsPositifs": {"type": "text", "fielddata": True},
+#                 "MotsNegatifsifs": {"type": "text", "fielddata": True},
+#                 "Sentiment": {"type": "text", "fielddata": True},
+#             }
+#         }
+#     }
+# #
 
 
 # Créer une instance Elasticsearch avec le nom d'utilisateur et le mot de passe
@@ -37,9 +55,31 @@ try:
         # Convert the date to the desired format (yyyy-MM-dd)
         doc["Date"] = datetime.datetime.strptime(doc["Date"], "%B %d, %Y").strftime("%Y-%m-%d")
 
+        # detection de la langue du commentaire
+        commentaires_non_nuls = list(filter(lambda x: x is not None, doc["Commentaire"]))
+        lang = detect_language(commentaires_non_nuls)
+        doc["Langue"]=lang
+
+        # Recupérer les mots positifs et negatifs de la phrase
+        comment = commentaires_non_nuls[0]
+        mots_positifs, mots_negatifs = mots_pos_neg(comment, lang)
+        doc["MotsPositifs"]=mots_positifs
+        doc["MotsNegatifs"] = mots_negatifs
+
+        # Estimer le sentiment
+        if (len(mots_positifs) >0  and len(mots_negatifs) >0 ):
+            doc["Sentiment"]   = "neutre"
+        elif (len(mots_positifs) > 0):
+            doc["Sentiment"] = "positif"
+        elif (len(mots_negatifs) > 0):
+            doc["Sentiment"] = "negatif"
+        else:
+            doc["Sentiment"]   = "neutre"
+
+        # Indexation
         es.index(index=index_name, body=doc)
-    
-    
+
+
     # Contrôler le nombre de documents charger
      # Verify document count using the correct method
     count = es.search(index=index_name, body={"size": 0})["hits"]["total"]["value"]
