3551782e76ad1720c2db79afff8e00b4bf5d05b1
nguyenhn <hnhan.nguyen@gmail.com>
2024-04-03 23:08:35 +0200

    Liste des adjectifs de sentiments. Non utilisé. Utilisation du module NLP à la place

---
+++ elastic/Cstsentiments.py
@@ -0,0 +1,202 @@
+
+sentiments_positifs_fr = [
+  # Émotions positives
+  "joie",
+  "bonheur",
+  "amour",
+  "gratitude",
+  "satisfaction",
+  "confiance",
+  "espoir",
+  "sérénité",
+  "paix",
+
+  # Sentiments de bien-être
+  "bien-être",
+  "santé",
+  "vitalité",
+  "énergie",
+  "relaxation",
+  "confort",
+
+  # Sentiments d'accomplissement
+  "succès",
+  "fierté",
+  "accomplissement",
+  "reconnaissance",
+
+  # Sentiments d'affection
+  "amour",
+  "affection",
+  "tendresse",
+  "amitié",
+  "complicité",
+  "sympathie",
+
+  # Sentiments d'admiration
+  "admiration",
+  "respect",
+  "gratitude",
+  "reconnaissante",
+  "inspiration",
+  "émerveillement",
+
+  # Autres sentiments positifs
+  "enthousiasme",
+  "passion",
+  "joie de vivre",
+  "optimisme",
+  "créativité",
+  "générosité",
+]
+
+sentiments_negatifs_fr = [
+  # Émotions négatives
+  "tristesse",
+  "peur",
+  "colère",
+  "dégoût",
+  "déception",
+  "culpabilité",
+  "honte",
+  "jalousie",
+  "anxiété",
+  "stress",
+
+  # Sentiments de mal-être
+  "mal-être",
+  "maladie",
+  "fatigue",
+  "douleur",
+  "incapacité",
+  "solitude",
+  "rejet",
+  "abandon",
+
+  # Sentiments d'échec
+  "échec",
+  "incompétence",
+  "insuffisance",
+  "rejet",
+  "déception",
+  "frustration",
+  "amertume",
+
+  # Sentiments d'hostilité
+  "haine",
+  "rage",
+  "ressentiment",
+  "mépris",
+  "rancune",
+  "agressivité",
+  "violence",
+
+  # Autres sentiments négatifs
+  "ennui",
+  "apathie",
+  "désespoir",
+  "pessimisme",
+  "cynisme",
+  "négativité",
+]
+
+sentiments_positifs_en = [
+  # Émotions positives
+  "joy",
+  "happiness",
+  "love",
+  "gratitude",
+  "satisfaction",
+  "confidence",
+  "hope",
+  "serenity",
+  "peace",
+
+  # Sentiments de bien-être
+  "well-being",
+  "health",
+  "vitality",
+  "energy",
+  "relaxation",
+  "comfort",
+
+  # Sentiments d'accomplissement
+  "success",
+  "pride",
+  "accomplishment",
+  "recognition",
+
+  # Sentiments d'affection
+  "love",
+  "affection",
+  "tenderness",
+  "friendship",
+  "complicity",
+  "sympathy",
+
+  # Sentiments d'admiration
+  "admiration",
+  "respect",
+  "gratitude",
+  "thankfulness",
+  "inspiration",
+  "wonder",
+
+  # Autres sentiments positifs
+  "enthusiasm",
+  "passion",
+  "joie de vivre",
+  "optimism",
+  "creativity",
+  "generosity",
+]
+
+sentiments_negatifs = [
+  # Émotions négatives
+  "sadness",
+  "fear",
+  "anger",
+  "disgust",
+  "disappointment",
+  "guilt",
+  "shame",
+  "jealousy",
+  "anxiety",
+  "stress",
+
+  # Sentiments de mal-être
+  "unhappiness",
+  "illness",
+  "fatigue",
+  "pain",
+  "inability",
+  "loneliness",
+  "rejection",
+  "abandonment",
+
+  # Sentiments d'échec
+  "failure",
+  "incompetence",
+  "insufficiency",
+  "rejection",
+  "disappointment",
+  "frustration",
+  "bitterness",
+
+  # Sentiments d'hostilité
+  "hatred",
+  "rage",
+  "resentment",
+  "contempt",
+  "grudge",
+  "aggressiveness",
+  "violence",
+
+  # Autres sentiments négatifs
+  "boredom",
+  "apathy",
+  "despair",
+  "pessimism",
+  "cynicism",
+  "negativity",
+]
